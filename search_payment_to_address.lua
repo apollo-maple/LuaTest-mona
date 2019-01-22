a8319d65b713358e2f3fe6807ebe4bef6bc7ca00
apollo-maple <33923134+apollo-maple@users.noreply.github.com>
2019-01-23 01:02:44 +0900

    fix bugs and add OnInit
    fix logical operator
    fix variable
    add OnInit

--- search_payment_to_address.lua
+++ search_payment_to_address.lua
@@ -5,16 +5,18 @@ function SearchTransaction(txid, message)
 
     if tx_ret == true then
         for j, tx_val in pairs(tx_value["details"]) do
-            if(tx_val["address"] == address && tx_val["category"] == "receive")) then
+            if(tx_val["address"] == address) and (tx_val["category"] == "receive") then
                 print "---------------------------------------------------\n"
-                print (string.format(message." %f MONA.", val["amount"]))
+                print (string.format("%S %f MONA.", message, val["amount"]))
                 print "\n---------------------------------------------------"
             end
         end
     end
 end
 
-
+function OnInit()
+    print ("search_payment_to_address loaded !!")
+end
 
 function OnBlockNotify(initioalsync, hash)
     block_ret, block_value = coind.getblock(hash)
@@ -30,7 +32,4 @@ end
 
 function OnWalletNotify(txid)
     SearchTransaction(txid, "new transaction has come.")
-
 end
-
-
