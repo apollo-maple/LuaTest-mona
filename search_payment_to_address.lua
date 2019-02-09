68aea7a361e958ffebe5b04ff42c2aa851ad1867
apollo-maple <33923134+apollo-maple@users.noreply.github.com>
2019-02-09 23:35:01 +0900

    Use table for multi txout
    previous version cannot watch multi txout to the address on one transaction.
    By using table, it becomes possible.

--- search_payment_to_address.lua
+++ search_payment_to_address.lua
@@ -5,18 +5,16 @@ local address = "YOUR_ADDRESS"
 
 function SearchTransaction(txid)
     tx_ret, tx_value = coind.getrawtransaction(txid,true)
-    exist = false
-    mona_val = 0
+    result = {}
 
     if tx_ret == true then
         for j, tx_val in pairs(tx_value["vout"]) do
             if (tx_val["scriptPubKey"]["addresses"][0] == address) then
-                exist = true
-                mona_val = tx_val["value"]
+                table.insert(result,{mona_val=tx_val["value"],n_conf=tx_value["confirmations"]})
             end
         end
     end
-    return exist,mona_val,tx_value["confirmations"]
+    return result
 end
 
 function OnInit()
@@ -28,20 +26,22 @@ function OnBlockNotify(initioalsync, hash)
 
     if block_ret == true then
         for i, block_val in pairs(block_value["tx"]) do
-            exist,mona_val,n_conf = SearchTransaction(block_val)
-            if (exist==true) then
-                print (string.format("new tx has entered into block! %f MONA.", mona_val))
+            result = SearchTransaction(block_val)
+            for j, table in pairs(result) do
+                print (string.format("new tx has entered into block! %f MONA.", table["mona_val"]))
             end
         end
     end
 end
 
 function OnWalletNotify(txid)
-    exist,mona_val,n_conf = SearchTransaction(txid)
-    if (exist==true) and (n_conf==nil) then
-            print (string.format("new transaction! %f MONA.",  mona_val))
-    end
-    if (exist==true) and (n_conf==1) then
-            print (string.format("1 confirmation! %f MONA.", mona_val))
+    result = SearchTransaction(txid)
+    for i, table in pairs(result) do
+        if (table["n_conf"]==nil) then
+                print (string.format("new transaction! %f MONA.",  table["mona_val"]))
+        end
+        if (table["n_conf"]==1) then
+                print (string.format("1 confirmation! %f MONA.", table["mona_val"]))
+        end
     end
 end
